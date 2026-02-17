043bcbc23f2be9f782a67af593805fef3488dcdc
Alexander Verbruggen <alex@celerium.be>
2026-02-17 18:31:07 +0100

    Optimized string wrapping to avoid wrapping inside a word

--- typescript/tests/string-wrap.out.ts
+++ typescript/tests/string-wrap.out.ts
@@ -1,2 +1,2 @@
-const message = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incid"
-	+ "idunt ut labore et dolore magna aliqua";
+const message = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor "
+	+ "incididunt ut labore et dolore magna aliqua";
