560cd10f1636d98940df0ab8ae784db5cbdb481c
Alexander Verbruggen <alex@celerium.be>
2026-02-17 16:12:55 +0100

    Added support for constructor body formatting in java

---
+++ java/tests/nested-class.out.java
@@ -0,0 +1,18 @@
+public class McpResource {
+	public static class ServerStatus {
+		public boolean running;
+		public String command;
+		public JsonNode capabilities;
+		public JsonNode initialize;
+
+		public ServerStatus() {
+		}
+
+		public ServerStatus(boolean running, String command, JsonNode capabilities, JsonNode initialize) {
+			this.running = running;
+			this.command = command;
+			this.capabilities = capabilities;
+			this.initialize = initialize;
+		}
+	}
+}
