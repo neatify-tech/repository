1cd534b979ff8bb5b348adfe2817b3749ff673dd
Alexander Verbruggen <alex@celerium.be>
2026-02-17 17:46:01 +0100

    Added class support and more unary operators

---
+++ typescript/tests/void-this.in.ts
@@ -0,0 +1,8 @@
+export class McpViewElement extends HTMLElement {
+  private render(): void {
+    if (!this.isConnected) return;
+    const uri = this.getRootUri();
+    if (!uri) return;
+    void this.load(uri);
+  }
+}
