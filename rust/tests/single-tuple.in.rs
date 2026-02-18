16bd468cdde7e9ec83985b249650088f3c42eddc
Alexander Verbruggen <alex@celerium.be>
2026-02-18 10:18:14 +0100

    Fixed support for single tuples

---
+++ rust/tests/single-tuple.in.rs
@@ -0,0 +1,2 @@
+rule.call_within_context(&ctx_call, (node_ref.clone(),))
+                            .map_err(Box::<EvalAltResult>::from)?;
---
+++ rust/tests/single-tuple.out.rs
@@ -0,0 +1,1 @@
+rule.call_within_context(&ctx_call, (node_ref.clone(),)).map_err(Box::<EvalAltResult>::from)?;
