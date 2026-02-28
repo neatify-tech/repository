41dfbfb667fa69d6a51f2e9fec5ee1c16a5886af
Alexander Verbruggen <alex@celerium.be>
2026-02-28 08:32:41 +0100

    Fix for shorthand method definitions

---
+++ typescript/tests/function-shorthand.out.ts
@@ -0,0 +1,14 @@
+const scalarEditor = createDomEditor(
+	{
+		container: document.querySelector("#editor-scalar"),
+		value: scalarInitial,
+		schema: scalarSchema,
+		lenient: false,
+		onChange(next) {
+			outputScalar.textContent = JSON.stringify(next, null, 2)
+			window.localStorage.setItem("json-editor-demo-scalar", JSON.stringify(next))
+		},
+		classes,
+		defaultCollapsed: false
+	}
+)
