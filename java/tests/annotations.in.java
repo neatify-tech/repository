bb6feab3d8baeddd05a173364545c504fdef2dee
Alexander Verbruggen <alex@celerium.be>
2026-06-06 14:52:37 +0200

    Tiny fix for long method descriptor formatting

---
+++ java/tests/annotations.in.java
@@ -0,0 +1,9 @@
+class Test {
+       @Inject
+       @ManagedExecutorConfig(cleared = ThreadContext.TRANSACTION) // Avoid propagating a completed transaction to
+                                                                                                                               // post-commit async dispatch.
+       ManagedExecutor dispatchExecutor;
+
+       @Inject
+       LlmCallRegistry callRegistry;
+}
---
+++ java/tests/long-method-description.in.java
@@ -0,0 +1,4 @@
+void enqueueRun(Session session, WorkflowRun run, WorkflowDefinition definition, JsonNode definitionJson,
+			UUID userId, JsonNode payload) {
+      System.out.println("test!");
+			}
