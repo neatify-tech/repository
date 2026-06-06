ab521f6e88abbbe8552a12a5158bd40b530ec2d6
Alexander Verbruggen <alex@celerium.be>
2026-06-06 16:02:28 +0200

    remove trailing spaces in for loop

---
+++ java/tests/for-with-semicolon.in.java
@@ -0,0 +1,3 @@
+	for (Iterator<Map<String, Object>> requestIterator = deniedRequests.iterator(); requestIterator
+							.hasNext();  ) {
+}
---
+++ java/tests/for-with-semicolon.out.java
@@ -0,0 +1,2 @@
+for (Iterator<Map<String, Object>> requestIterator = deniedRequests.iterator(); requestIterator.hasNext();) {
+}
