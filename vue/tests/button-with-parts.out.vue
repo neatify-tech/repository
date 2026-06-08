31aa6b0d1fc62274cc25ccacd530623096d45a4f
Alexander Verbruggen <alex@celerium.be>
2026-06-08 21:31:50 +0200

    Embedded text

---
+++ vue/tests/button-with-parts.out.vue
@@ -0,0 +1,7 @@
+<button
+	class="control size-xs ghost"
+	type="button"
+	@click="workerPaneOpen = true">
+	Workers: {{ selectedWorkerProgress.done || 0 }}/{{ selectedWorkerProgress.total }} done
+	<span v-if="selectedWorkerProgress.failed">· {{ selectedWorkerProgress.failed }} failed</span>
+</button>
